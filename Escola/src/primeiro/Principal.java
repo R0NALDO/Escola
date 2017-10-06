ea390423893e92b8e7933ecbd930ee2b8e32c4ce
Ronaldo Pereira <ox99709179@gmail.com>
2017-10-06 20:48:21 -0300

    add nome, email e matricula, e impressão

--- Escola/src/primeiro/Principal.java
+++ Escola/src/primeiro/Principal.java
@@ -6,17 +6,17 @@ public class Principal {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	
-	Aluno fulano = new Aluno("123");
+	Aluno fulano = new Aluno("Ana");
 	
-	System.out.println(	fulano.matricula+"\n"+
-						fulano.nome+"\n"+
-						fulano.email);
-	
-		for(Nota n : fulano.notas){
-			System.out.println(	 n.disciplina.codigo+" - "
-								+n.disciplina.nome+" - "
-								+n.termo+" - "
-								+n.valor);
-		}
+//	System.out.println(	fulano.matricula+"\n"+
+//						fulano.nome+"\n"+
+//						fulano.email);
+//	
+//		for(Nota nt : fulano.notas){
+//			System.out.println(	 	 nt.termo+" - "
+//									+nt.valor+" - "
+//									+nt.disciplina.codigo+" - "
+//									+nt.disciplina.nome+" - ");
+//		}
 	}
 }
