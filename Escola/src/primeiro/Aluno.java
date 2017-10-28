c8be445e9a67c89bb53303a93ff937d0daf0435e
Ronaldo Pereira <ox99709179@gmail.com>
2017-10-27 23:35:29 -0200

    atualizando

--- Escola/src/primeiro/Aluno.java
+++ Escola/src/primeiro/Aluno.java
@@ -15,40 +15,35 @@ public class Aluno {
 	public String email;
 	public ArrayList<Nota> notas = new ArrayList<Nota>();
 	
-	public Aluno(String nomex) throws ClassNotFoundException, SQLException{
+	public Aluno(String matricula) throws ClassNotFoundException, SQLException{
 		
-		Class.forName(			"com.mysql.jdbc.Driver");
-		String 		stgCnxo = 	"jdbc:mysql://localhost/escola?autoReconnect=true&useSSL=false";
-		Connection 	cnxo = DriverManager.getConnection(stgCnxo,"root", "");
-		Statement 	cmdo = cnxo.createStatement();
-//		Scanner 	ler = new Scanner(System.in);
-		
-//		String 		sql =     "SELECT N.NOTA, N.TERMO, D.CODIGO_DISCIPLINA, D.DISCIPLINA FROM NOTAS N INNER JOIN DISCIPLINA D ON N.CODIGO_DISCIPLINA = D.CODIGO_DISCIPLINA WHERE MATRICULA = "+nomex;
-		String 		sqlAll = "SELECT N.NOTA, N.TERMO, D.CODIGO_DISCIPLINA, D.DISCIPLINA, A.NOME, A.MATRICULA, A.EMAIL FROM NOTAS N LEFT JOIN DISCIPLINA D ON N.CODIGO_DISCIPLINA =  D.CODIGO_DISCIPLINA LEFT JOIN ALUNO A ON A.MATRICULA = N.MATRICULA";
-
-		ResultSet 	rstst = cmdo.executeQuery(sqlAll);
-	
-		while (rstst.next()) {
-			Nota 		nota = new Nota();
-			Disciplina 	disciplina = new Disciplina();
-			
-			disciplina.codigo = rstst.getString("CODIGO_DISCIPLINA");
-			disciplina.nome = 	rstst.getString("DISCIPLINA");
-			nota.disciplina = 	disciplina;
-			nota.termo = 		rstst.getString("TERMO");
-			nota.valor = 		rstst.getDouble("NOTA");
-			
-			notas.add(nota);
-			
-			this.matricula = 	rstst.getString("MATRICULA");
-			this.nome = 		rstst.getString("NOME");
-			this.email = 		rstst.getString("EMAIL");
-			
-			if (nomex.equals(this.nome)) {
-				System.out.println(this.nome + " " + this.email + " " + this.matricula + "\n " + nota.valor + " "
-						+ nota.termo + " " + disciplina.codigo + " " + disciplina.nome);
-			}
-		}
-		cnxo.close();
+ 		Class.forName(			"com.mysql.jdbc.Driver");
+ 		String 		stgCnxo = 	"jdbc:mysql://localhost/escola?autoReconnect=true&useSSL=false";
+ 		Connection 	cnxo = DriverManager.getConnection(stgCnxo,"root", "");
+ 		Statement 	cmdo = cnxo.createStatement();
+ 		Scanner 	ler = new Scanner(System.in);
+ 		String 		sql =     "SELECT N.NOTA, N.TERMO, D.CODIGO_DISCIPLINA, D.DISCIPLINA "
+ 							+ "FROM NOTAS N INNER JOIN DISCIPLINA D ON N.CODIGO_DISCIPLINA = D.CODIGO_DISCIPLINA "
+ 							+ "WHERE MATRICULA = "+matricula;
+ 
+ 		ResultSet rstst = cmdo.executeQuery(sql);
+ 		
+ 		rstst.next(); //pula primeira linha, pois é nula 
+ 	
+ 		Nota 		nota = new Nota();
+ 		Disciplina 	disciplina = new Disciplina();
+ 		
+ 		disciplina.codigo = rstst.getString("CODIGO_DISCIPLINA");
+ 		disciplina.nome = 	rstst.getString("DISCIPLINA");
+ 		nota.disciplina = 	disciplina;
+ 		nota.termo = 		rstst.getString("TERMO");
+ 		nota.valor = 		rstst.getDouble("NOTA");
+ 		notas.add(nota);
+ 		
+ 		this.matricula = matricula;
+ 		this.nome = "ronaldo";
+ 		this.email = "ron@gmail.com";
+ 		
+ 		cnxo.close();
 	}
 }
--- Escola/src/primeiro/Principal.java
+++ Escola/src/primeiro/Principal.java
@@ -6,17 +6,17 @@ public class Principal {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	
-	Aluno fulano = new Aluno("Ana");
+	Aluno fulano = new Aluno("123");
 	
-//	System.out.println(	fulano.matricula+"\n"+
-//						fulano.nome+"\n"+
-//						fulano.email);
-//	
-//		for(Nota nt : fulano.notas){
-//			System.out.println(	 	 nt.termo+" - "
-//									+nt.valor+" - "
-//									+nt.disciplina.codigo+" - "
-//									+nt.disciplina.nome+" - ");
-//		}
+	System.out.println(	fulano.matricula+"\n"+
+						fulano.nome+"\n"+
+						fulano.email);
+	
+		for(Nota nt : fulano.notas){
+			System.out.println(	 	 nt.termo+" - "
+									+nt.valor+" - "
+									+nt.disciplina.codigo+" - "
+									+nt.disciplina.nome+" - ");
+		}
 	}
 }
